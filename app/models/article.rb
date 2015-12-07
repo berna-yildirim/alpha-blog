4f82e2cc5ad6da1ada8463dda6183051349d32c9
berna yildirim <byildirim@protel.com.tr>
2015-12-07 22:26:18 +0200

    Fix create, add flash and error messages, create show

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true, length: { min: 3, maximum: 50 }
-  validates :description, presence:true, length: { min: 10, maximum: 300 }
+  validates :description, presence: true, length: { min: 10, maximum: 300 }
 
 end
