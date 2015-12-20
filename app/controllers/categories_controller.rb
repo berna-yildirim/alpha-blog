cd7a0a2e3c5c15f34584bed556c88f8bcf748b37
berna yildirim <byildirim@protel.com.tr>
2015-12-20 15:54:28 +0200

    Update all views of categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,9 +3,11 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
+
   def new
     @category = Category.new
   end
+
   def create
     @category = Category.new(category_params)
     if @category.save
@@ -15,12 +17,17 @@ class CategoriesController < ApplicationController
       render 'new'
     end
   end
+
   def show
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
+
   private
   def category_params
     params.require(:category).permit(:name)
   end
+
   def require_admin
     if !logged_in? || (logged_in? and !current_user.admin?)
       flash[:danger] = "Only admins can perform that action"
